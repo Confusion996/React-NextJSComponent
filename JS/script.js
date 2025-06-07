317e0f3588b29226e36cf77b72c1bce29989c173
Barnes <170465826+Confusion996@users.noreply.github.com>
2025-06-07 12:42:41 +0800

    initial commit
    html and js change

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+//variable
+let name = "gwapo";
+
+//arrays
+const array = ["hatdog","itlog","rise"];
+document.getElementById("pamahaw").innerHTML= array;
+
+//object
+let shrimpfriedrice={
+    carbohydrates: rice,
+    protien: shrimp,
+    status: Burnt,
+};
+
+//functions
+function cookRice(shrimpfriedrice){
+    
+}
