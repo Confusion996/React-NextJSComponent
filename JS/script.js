dcf5bc0fade50d6898ccd95e75e975a897e2e99b
Barnes <170465826+Confusion996@users.noreply.github.com>
2025-06-07 17:28:32 +0800

    javasktip

--- JS/script.js
+++ JS/script.js
@@ -3,16 +3,33 @@ let name = "gwapo";
 
 //arrays
 const array = ["hatdog","itlog","rise"];
+//DOM
 document.getElementById("pamahaw").innerHTML= array;
 
 //object
-let shrimpfriedrice={
-    carbohydrates: rice,
-    protien: shrimp,
-    status: Burnt,
+let shrimpfriedrice = {
+    carbohydrates: "rice",
+    protien: "shrimp",
+    status: function(){
+        return "you made shrimp fried rice";
+    }
 };
 
 //functions
-function cookRice(shrimpfriedrice){
-    
+function cookRice(){
+    document.getElementById("cook").innerHTML= shrimpfriedrice.status();
+
+    const shrimp = getElementById("shrimp");
+
+   const img = document.createElement("img");
+   img.src= "shrimpfriedrice.png";
+   img.id="shrmppic";
+   img.alt="its a literal shrimp frying rice";
+
+   shrimp.appendChild(img);
 }
+
+//event handling
+document.getElementById("fry").onclick = cookRice;
+
+
